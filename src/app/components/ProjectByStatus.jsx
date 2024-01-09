b53954b5a2c05d01f07d43aade466986722ea4df
kshamarim214186 <kshamarim@gmail.com>
2024-01-09 10:32:22 +0530

    update

---
+++ src/app/components/ProjectByStatus.jsx
@@ -0,0 +1,54 @@
+"use client";
+import ListItems from "../UI/ListItems";
+import styles from "../scss/developer.module.scss";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import CustomPagination from "@/app/components/CustomPagination";
+
+export default async function ProjectByStatus({ itemObj, page, currentpage }) {   
+
+   const projects = await getProjectByStatus(itemObj.id, page);
+   const properties = projects.projectbystatus;
+   const message = projects.message;
+   const totalrecords = projects.totalrecords;
+   const perpagerecord = projects.perpagerecord;
+   const number_of_page = Math.ceil(totalrecords / perpagerecord); 
+   //console.log(properties);
+   return ( 
+      <>
+         <div className={styles.counts}>
+            {Number(totalrecords)} {Number(totalrecords) <= 1 ? "Property" : "Properties"} Found
+         </div>          
+         {properties.map(function(property) {
+            return (
+               <ListItems itemObj={property} key={property.propId}/>
+            );
+         })}
+         {message=='success' &&
+            <div className="col-12">
+               <div className={styles.paginContainer}>
+                 <CustomPagination totalrecord={totalrecords} pagename={itemObj.urlval} currentpage={currentpage} numberofpage={number_of_page} />
+               </div>
+            </div>
+         }
+      </>
+  );
+}
+
+async function getProjectByStatus(statusId,page) {
+   //console.log(floorcatid);
+   const formData = new URLSearchParams();
+
+   formData.append('status_id', statusId);
+   formData.append('token1', process.env.token1);
+   formData.append('token2', process.env.token2);
+   formData.append('page', page);
+   const finalresult = await fetch(process.env.API_URL+'property-status/getprojectbystatus/', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formData,
+   });   
+   return await finalresult.json();
+}
---
+++ src/app/property-type/[statusurl]/page.jsx
@@ -0,0 +1,120 @@
+"use client";
+import { useSearchParams } from 'next/navigation';
+import { NextSeo } from "next-seo";
+import Image from "next/image";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import styles from "../../scss/properties.module.scss";
+import Pagination from "react-bootstrap/Pagination";
+import { useState } from "react";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilterList } from "@fortawesome/pro-regular-svg-icons";
+import CustomButton from "@/app/UI/CustomButton";
+import Accordion from "react-bootstrap/Accordion";
+import SortFilter from "@/app/UI/sort-filter";
+import ProjectByStatus from "../../components/ProjectByStatus";
+import Filter from "../../components/Filter";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import NotFound from "@/app/components/NotFound";
+import getHomeCompleteData from "../../api/getHomeCompleteData";
+import getLocationType from "../../api/getLocationType";
+
+
+export default async function PropertyStatus({ params: { statusurl } }) {
+
+   const searchParams = useSearchParams() 
+   const page = searchParams.get('page') ? searchParams.get('page') : "";
+   const currentpage = searchParams.get('page') ? searchParams.get('page') : "1";
+
+   const statusData = getSingleStatusDetails(statusurl);
+   const statusResult = await statusData
+   const message = statusResult.message;
+   const statsData = statusResult.statusdata;
+   //console.log(locData)
+
+   const props = getHomeCompleteData();
+   const result = await props;
+
+   const residential = getLocationType('residential');
+   const resData = await residential;
+   const residentialData = resData.loctype;
+
+
+   const commercial = getLocationType('commercial');
+   const commData = await commercial;
+   const commercialData = commData.loctype;
+
+   return (
+      <>
+      <Header resultHeader={result} commercialData={commercialData} residentialData={residentialData} />
+      {message=='success' ?
+         <main className={`${styles.container} container-xl`}>
+            <title>{statsData.seotitle}</title>
+            <meta name="description" content={statsData.seodesc} />
+            <Breadcrumb className={styles.bredcurmb}>
+                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href={statsData.url}>{statsData.name}</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="row">
+               <div className="col-lg-4">
+                  <div className={`${styles.container__left} sticky-top`}>                     
+                     <Filter />
+                  </div>
+               </div>
+               <div className="col-lg-8">
+                  <div className={styles.container__right}>
+                    <div className={styles.listInfo}>
+                      <div className="">
+                        <div className="h1">{statsData.name}</div>                        
+                      </div>
+                      <div className={styles.sort}>
+                        <SortFilter />
+                      </div>
+                    </div>
+                    <div className={styles.allList}>
+                        <ProjectByStatus itemObj={statsData} page={page} currentpage={currentpage} />
+                    </div>
+                  </div>
+               </div>               
+            </div>
+            <div className="row">
+               <div className="col-lg-12">
+                  <div className={styles.overview}>
+                     <h1>{statsData.name}</h1>
+                     <p>{statsData.shortdesc}</p>
+                     <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="1">
+                           <Accordion.Body dangerouslySetInnerHTML={{ __html: statsData.fulldesc }}></Accordion.Body>
+                           <Accordion.Header as={"div"}></Accordion.Header>
+                        </Accordion.Item>
+                    </Accordion>
+                  </div>
+               </div>
+            </div>
+         </main>
+         : <NotFound />
+      }
+      <Footer resultFooter={result} commercialData={commercialData} residentialData={residentialData} />
+      </>
+   );
+}
+
+async function getSingleStatusDetails(statusurl) {
+   //const  apiUrl = 'https://www.api.hcorealestates.org/';
+   const formData = new URLSearchParams();
+
+   formData.append('statusurl', statusurl);
+   formData.append('token1', process.env.token1);
+   formData.append('token2', process.env.token2);
+   const finalresult = await fetch(process.env.API_URL+'property-status/', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formData,
+   });  
+   return finalresult.json();
+}
